370acf85fe40653d048821556ddc3e71aa585640
Owen Davies <tod11@doc.ic.ac.uk>
2013-05-20 18:11:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruralcloud::Application.config.secret_token = '82618747a3a18cd57953117862e357f9570a950c3699da68ace315a8aec8e3d83c7b8c03c1ca9eeb3be6672ae34cece7212d39b854cc7801ae5ad06f67c5a02e'
