067f1051611f62c4004aca351c6a31c40c66b34f
Daniel Hertz <daniel@dhertz.com>
2013-05-23 23:09:24 +0100

    tieded up date selection and form now validates.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,23 @@
 class Challenge < ActiveRecord::Base
+  validates :description, :difficulty, :owner, :spec, :presence => true
+  validate :not_in_past, :later_than_start
   attr_accessible :description, :difficulty, :endtime, :owner, :spec, :starttime
 
   has_and_belongs_to_many :users
+
+ def not_in_past
+  if !starttime.blank? and starttime < DateTime.now
+    errors.add(:starttime, "Start time can't be in the past")
+  end
+  if !endtime.blank? and endtime < DateTime.now
+    errors.add(:endtime, "End time can't be in the past")
+  end
+end
+
+def later_than_start
+  if !starttime.blank? and !endtime.blank? and starttime >= endtime
+    errors.add(:endtime, "Start time can't be after or equal to end time")
+  end
+end
+
 end
