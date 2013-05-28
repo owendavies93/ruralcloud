5ea75953839780503759a25cda6205a0a76e2a0c
Owen Davies <tod11@doc.ic.ac.uk>
2013-05-29 00:37:54 +0100

    Implemented tests file parsing on file upload

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,7 +1,7 @@
 class Challenge < ActiveRecord::Base
   validates :description, :owner, :spec, :test_suite, :presence => true
   validates :difficulty, :inclusion => {:in => 0..10, :message => "isn't between 1 and 10"}
-  validate :not_in_past, :later_than_start, :not_invalid
+  validate :not_in_past, :later_than_start, :not_invalid, :valid_test_suite
   attr_accessible :description, :difficulty, :endtime, :owner, :spec, :starttime, :log, :test_suite
 
   has_attached_file :test_suite
@@ -36,10 +36,25 @@ class Challenge < ActiveRecord::Base
   end
 
   def valid_test_suite
-    File.open(test_suite.queued_for_write[:original].path) do |f|
-      f.each do |l|
-        puts l
+    cur_test_line = 0
+    if test_suite.queued_for_write[:original] != nil
+      File.open(test_suite.queued_for_write[:original].path) do |f|
+        f.each_with_index do |line, index|
+          if index == 0
+            if line[0] != '>'
+              errors.add(:test_suite, "Test suite must start with test")
+            end
+          else
+            if line[0] == '>' && (index == cur_test_line + 1 || f.eof?)
+              errors.add(:test_suite, "All tests must have at least one possible output")
+            elsif line[0] == '>'
+              cur_test_line = index
+            end
+          end
+        end
       end
+    else
+      errors.add(:test_suite, "Must submit test suite file")
     end
   end
 
