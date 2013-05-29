95f96fb80fe15c22807919c1e44794fbda7f8e82
Owen Davies <tod11@doc.ic.ac.uk>
2013-05-29 02:00:00 +0100

    Fixed gravatar bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,6 @@ module ApplicationHelper
 
   def avatar user, size
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&f=y&d=identicon"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=identicon"
   end
 end
