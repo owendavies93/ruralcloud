58e2cd2f04979c9bddfad0fff9be0b80f13e7734
Owen Davies <tod11@doc.ic.ac.uk>
2013-05-29 01:27:27 +0100

    Added S3 support for file upload in production

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem 'certified'
 
 # File upload
 gem 'paperclip'
+gem 'aws-sdk'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,14 @@ Ruralcloud::Application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+
+  # config for file upload to S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
